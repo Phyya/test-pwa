4d4816dd4bc86eb828002dd06f1c62a673349ef2
phyya <phaboolous11@gmail.com>
2023-05-14 16:58:50 +0100

    check installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import RoutesComponent from './routes/routes';
 import './App.css';
 
 function App() {
+	useEffect(() => {
+		window.addEventListener('appinstalled', (event) => {
+			navigate('/optiverse');
+		});
+	});
 	return (
 		<div className="App">
 			<RoutesComponent />
