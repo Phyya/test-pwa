4aaddaadb28de881d7ed94177408b8d94b67ef6e
phyya <phaboolous11@gmail.com>
2023-06-06 07:50:41 +0100

    fixed balance

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Input.module.css';
 
 const Input = ({ icon, icon2, type, onChange, placeholder }) => {
+	const [showPassword, setShowPassword] = useState(false);
+	const [inputType, setInputType] = useState(type);
+	const handleIconClick = () => {
+		if (inputType != 'password') {
+			setInputType('text');
+		} else {
+			setInputType('password');
+		}
+	};
 	return (
 		<div className={style.input_container}>
 			<span>{icon}</span>
 			<input
+				type={inputType}
 				placeholder={placeholder}
 				onChange={(e) => onChange(e.target.value)}
 			/>
-			<span>{icon2}</span>
+			<span onClick={handleIconClick}>{icon2}</span>
 		</div>
 	);
 };
